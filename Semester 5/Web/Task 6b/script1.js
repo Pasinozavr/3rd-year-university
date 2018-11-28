28b1c311ca02d392d406e09be481b0c3b44605e0
Pasinozavr <96pasha@ukr.net>
2018-11-29 00:06:27 +0100

    Add files via upload

---
+++ Semester 5/Web/Task 6b/script1.js
@@ -0,0 +1,41 @@
+﻿$(function(){
+	$(document).ready(function(){
+		
+		
+	});
+	$("#send").click(function(){
+		
+		//var s = $('form').serializeArray();
+		//console.log(document.forms["forma"].elements["login"].value);
+		
+		if(document.forms["forma"].elements["login"].value == "")
+		{
+			alert("Я так и знал. Нельзя доверять человеку без имени.");return;
+		}
+		
+		
+		
+		
+		
+		
+		$.ajax({
+            url: 'server_login.php',
+            data: 'login='+document.forms["forma"].elements["login"].value
+            + '&password='+document.forms["forma"].elements["pass"].value,
+            dataType: 'json',
+            type: 'post',
+            complete: function(){
+
+            },
+            error:function(xhr){
+                console.error( xhr.responseText );
+				$("#answer").html(xhr.responseText);
+            },
+            
+
+            
+        })
+
+	});
+	
+});
